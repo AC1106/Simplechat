b4e06aae2617b8a62a4820fa3ef1d9e382db7942
AC1106 <ariel86.chang@gmail.com>
2024-05-23 01:59:56 +0800

    Add files via upload

---
+++ Server/server.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define SERVER_PORT XXXX
+#define BUFFER_SIZE XXXX
+
+void handleClient(SOCKET clientSocket) {
+    char buffer[BUFFER_SIZE];
+    int bytesReceived;
+
+    while ((bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE, 0)) > 0) {
+        buffer[bytesReceived] = '\0';
+        std::cout << "Client: " << buffer << std::endl;
+
+        send(clientSocket, buffer, bytesReceived, 0);
+    }
+
+    closesocket(clientSocket);
+}
+
+int main() {
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        std::cerr << "Error initializing Winsock: " << result << std::endl;
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    result = bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (result == SOCKET_ERROR) {
+        std::cerr << "Error binding socket: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    result = listen(serverSocket, SOMAXCONN);
+    if (result == SOCKET_ERROR) {
+        std::cerr << "Error listening on socket: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server is listening on port " << SERVER_PORT << std::endl;
+
+    SOCKET clientSocket;
+    sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+
+    while ((clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientAddrSize)) != INVALID_SOCKET) {
+        std::cout << "Client connected." << std::endl;
+        handleClient(clientSocket);
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
